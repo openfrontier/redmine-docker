71adcb9bf7774600ecfa79dadb4203dd3ee7bb11
zsx <thinkernel@gmail.com>
2015-05-26 08:37:54 +0000

    Use gerrit-volume instead of gerrit with ro privilege.

--- createRedmine.sh
+++ createRedmine.sh
@@ -4,6 +4,7 @@ set -e
 PG_REDMINE_NAME=${PG_REDMINE_NAME:-pg-redmine}
 REDMINE_NAME=${REDMINE_NAME:-redmine}
 REDMINE_IMAGE_NAME=${REDMINE_IMAGE_NAME:-sameersbn/redmine}
+GERRIT_VOLUME=${GERRIT_VOLUME:-gerrit-volume}
 
 docker run \
 --name ${PG_REDMINE_NAME} \
@@ -26,5 +27,5 @@ docker run \
 -e DB_PASS=redmine \
 -e REDMINE_RELATIVE_URL_ROOT=/redmine \
 -e REDMINE_FETCH_COMMITS=hourly \
---volumes-from gerrit \
+--volumes-from ${GERRIT_VOLUME}:ro \
 -d ${REDMINE_IMAGE_NAME}
