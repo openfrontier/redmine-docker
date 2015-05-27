9ff1efb4051ab76c4cf7ab98b4bfaa8098ffdc28
zsx <thinkernel@gmail.com>
2015-05-27 08:53:15 +0000

    Add data volume for redmine container.
    init should be removed after pull request is accepted.

--- createRedmine.sh
+++ createRedmine.sh
@@ -4,8 +4,10 @@ set -e
 PG_REDMINE_NAME=${PG_REDMINE_NAME:-pg-redmine}
 REDMINE_NAME=${REDMINE_NAME:-redmine}
 REDMINE_IMAGE_NAME=${REDMINE_IMAGE_NAME:-sameersbn/redmine}
+REDMINE_VOLUME=${REDMINE_VOLUME:-redmine-volume}
 GERRIT_VOLUME=${GERRIT_VOLUME:-gerrit-volume}
 
+# Start PostgreSQL.
 docker run \
 --name ${PG_REDMINE_NAME} \
 -P \
@@ -19,13 +21,20 @@ while [ -z "$(docker logs ${PG_REDMINE_NAME} 2>&1 | grep 'autovacuum launcher st
     sleep 1
 done
 
+# Create Redmine volume.
+docker run \
+--name ${REDMINE_VOLUME} \
+-v ~/redmine-docker/init:/app/init \
+${REDMINE_IMAGE_NAME} \
+app:volume
+
+# Start Gerrit.
 docker run \
 --name=${REDMINE_NAME} \
 --link pg-redmine:postgresql \
 -e DB_NAME=redmine \
--e DB_USER=redmine \
--e DB_PASS=redmine \
 -e REDMINE_RELATIVE_URL_ROOT=/redmine \
 -e REDMINE_FETCH_COMMITS=hourly \
+--volumes-from ${REDMINE_VOLUME} \
 --volumes-from ${GERRIT_VOLUME}:ro \
 -d ${REDMINE_IMAGE_NAME}
--- destroyRedmine.sh
+++ destroyRedmine.sh
@@ -2,8 +2,10 @@
 
 PG_REDMINE_NAME=${PG_REDMINE_NAME:-pg-redmine}
 REDMINE_NAME=${REDMINE_NAME:-redmine}
+REDMINE_VOLUME=${REDMINE_VOLUME:-redmine-volume}
 
 docker stop ${REDMINE_NAME}
 docker rm -v ${REDMINE_NAME}
+docker rm -v ${REDMINE_VOLUME}
 docker stop ${PG_REDMINE_NAME}
 docker rm -v ${PG_REDMINE_NAME}
