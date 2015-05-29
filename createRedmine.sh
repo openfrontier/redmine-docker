45ed6603cfc07038192bd857a9b3edca9aefe2a6
mengzhaopeng <qiuranke@gmail.com>
2015-05-29 17:04:47 +0800

    redmine container setup

--- createRedmine.sh
+++ createRedmine.sh
@@ -7,16 +7,15 @@ REDMINE_IMAGE_NAME=${REDMINE_IMAGE_NAME:-sameersbn/redmine}
 REDMINE_VOLUME=${REDMINE_VOLUME:-redmine-volume}
 GERRIT_VOLUME=${GERRIT_VOLUME:-gerrit-volume}
 
-REDMINE_CONF1=redmine-init-system.sql
-REDMINE_CONF2=redmine-init-demo.sql
-HOST_IP=`ifconfig eth0|sed -n '/inet addr/s/^[^:]*:\([0-9.]\{7,15\}\) .*/\1/p'`
+REDMINE_SYS_DATA_SQL=redmine-init-system.sql
+REDMINE_DEMO_DATA_SQL=redmine-init-demo.sql
 INIT_DATE=`date +%Y-%m-%d\ %H:%M:%S.%N|cut -c 1-26`
 
 # Redmine init data
-sed -e "s/{INIT_DATE}/${INIT_DATE}/g" ~/redmine-docker/${REDMINE_CONF1}.template > ~/redmine-docker/${REDMINE_CONF1}
-sed -i "s/{HOST_IP}/${HOST_IP}/g" ~/redmine-docker/${REDMINE_CONF1}
-sed -i "s/{LDAP_ACCOUNTBASE}/${LDAP_ACCOUNTBASE}/g" ~/redmine-docker/${REDMINE_CONF1}
-sed -e "s/{INIT_DATE}/${INIT_DATE}/g" ~/redmine-docker/${REDMINE_CONF2}.template > ~/redmine-docker/${REDMINE_CONF2}
+sed -e "s/{INIT_DATE}/${INIT_DATE}/g" ~/redmine-docker/${REDMINE_SYS_DATA_SQL}.template > ~/redmine-docker/${REDMINE_SYS_DATA_SQL}
+sed -i "s/{HOST_IP}/${LDAP_SERVER}/g" ~/redmine-docker/${REDMINE_SYS_DATA_SQL}
+sed -i "s/{LDAP_ACCOUNTBASE}/${LDAP_ACCOUNTBASE}/g" ~/redmine-docker/${REDMINE_SYS_DATA_SQL}
+sed -e "s/{INIT_DATE}/${INIT_DATE}/g" ~/redmine-docker/${REDMINE_DEMO_DATA_SQL}.template > ~/redmine-docker/${REDMINE_DEMO_DATA_SQL}
 
 # Start PostgreSQL.
 docker run \
@@ -25,8 +24,8 @@ docker run \
 -e POSTGRES_USER=redmine \
 -e POSTGRES_PASSWORD=redmine \
 -e POSTGRES_DB=redmine \
--v ~/redmine-docker/${REDMINE_CONF1}:/${REDMINE_CONF1}:ro \
--v ~/redmine-docker/${REDMINE_CONF2}:/${REDMINE_CONF2}:ro \
+-v ~/redmine-docker/${REDMINE_SYS_DATA_SQL}:/${REDMINE_SYS_DATA_SQL}:ro \
+-v ~/redmine-docker/${REDMINE_DEMO_DATA_SQL}:/${REDMINE_DEMO_DATA_SQL}:ro \
 -d postgres
 
 while [ -z "$(docker logs ${PG_REDMINE_NAME} 2>&1 | grep 'autovacuum launcher started')" ]; do
